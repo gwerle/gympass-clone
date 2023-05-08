38d7389823784d9b5248f6ed582f05c611101696
Gustavo Werle <gustavo.werle2@gmail.com>
2023-05-08 00:11:05 -0300

    feat: add first migrations and create users endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    email: z.string().email(),
+    name: z.string(),
+    password: z.string().min(6),
+  })
+
+  const { email, name, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      email,
+      name,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+})
