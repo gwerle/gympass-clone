6a76489a1ae5af0ff4d3e4b091a5c923a5064fa2
Gustavo Werle <gustavo.werle2@gmail.com>
2024-09-21 18:44:36 -0300

    chore: move setErrorHandler to app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-import { ZodError } from 'zod'
 import { app } from './app'
 import { env } from './env'
 
@@ -10,21 +9,5 @@ app
   .then(() => {
     console.log('HTTP Server Running!')
   })
-
-app.setErrorHandler((error, _, reply) => {
-  if (error instanceof ZodError) {
-    return reply
-      .status(400)
-      .send({ message: 'Validation error.', issues: error.format() })
-  }
-
-  if (env.NODE_ENV !== 'production') {
-    console.error(error)
-  } else {
-    // TODO: Apply log with Datadog
-  }
-
-  return reply.status(500).send({ message: 'Internal server error.' })
-})
 
 export default app
