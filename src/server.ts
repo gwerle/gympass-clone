41703cb6b41bf9fe25f58deaf2f67e5b3784521a
Gustavo Werle <gustavo.werle2@gmail.com>
2024-09-21 18:42:48 -0300

    feat: apply general error handler

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import { ZodError } from 'zod'
 import { app } from './app'
 import { env } from './env'
 
@@ -9,5 +10,21 @@ app
   .then(() => {
     console.log('HTTP Server Running!')
   })
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Apply log with Datadog
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
 
 export default app
